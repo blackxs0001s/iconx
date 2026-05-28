d80968c7c19cf8f8b6ec15716b5b7e6d46503e67
blackxs0001s <blackxs0001s@gmail.com>
2026-05-28 21:05:22 +0700

    Create iconkit_r.js

---
+++ iconkit_r.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  const base =
+    "https://cdn.jsdelivr.net/gh/blackxs0001s/iconx@main/svg_r/";
+
+  document.querySelectorAll("[class*='ji-']").forEach(el => {
+
+    const cls = [...el.classList]
+      .find(c => c.startsWith("ji-"));
+
+    if (!cls) return;
+
+    const icon = cls.replace("ji-", "");
+
+    el.style.display = "inline-block";
+    el.style.width = "1em";
+    el.style.height = "1em";
+    el.style.minWidth = "1em";
+    el.style.minHeight = "1em";
+
+    el.style.backgroundColor = "currentColor";
+    el.style.verticalAlign = "-0.125em";
+
+    el.style.mask =
+      `url("${base}${icon}.svg") center / contain no-repeat`;
+
+    el.style.webkitMask =
+      `url("${base}${icon}.svg") center / contain no-repeat`;
+
+  });
+
+});
