d1685a1b0aed32d4d48385b0ffd9d95b2213409a
blackxs0001s <blackxs0001s@gmail.com>
2026-05-29 01:26:21 +0700

    Update iconkit.js

--- iconkit.js
+++ iconkit.js
@@ -1,16 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  const base =
-    "https://cdn.jsdelivr.net/gh/blackxs0001s/iconx@main/svg/";
+  const repo =
+    "https://cdn.jsdelivr.net/gh/blackxs0001s/iconx@main/";
+
+  const styles = {
+    "ji-regular": "svg_r/svg/",
+    "ji-bold": "svg_b/svg/"
+  };
 
   document.querySelectorAll("[class*='ji-']").forEach(el => {
 
+    let styleClass = null;
+
+    Object.keys(styles).forEach(c => {
+      if (el.classList.contains(c)) {
+        styleClass = c;
+      }
+    });
+
+    if (!styleClass) return;
+
     const cls = [...el.classList]
-      .find(c => c.startsWith("ji-"));
+      .find(c =>
+        c.startsWith("ji-") &&
+        c !== "ji-regular" &&
+        c !== "ji-bold"
+      );
 
     if (!cls) return;
 
     const icon = cls.replace("ji-", "");
+
+    const base =
+      repo + styles[styleClass];
 
     el.style.display = "inline-block";
     el.style.width = "1em";
