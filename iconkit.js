9a0984083021d43db00139217414e442ef71487b
blackxs0001s <blackxs0001s@gmail.com>
2026-05-28 19:57:08 +0700

    Add files via upload

--- iconkit.js
+++ iconkit.js
@@ -1,28 +1,18 @@
 (function () {
-
-  const base =
-    "https://cdn.jsdelivr.net/gh/blackxs0001s/iconx@main/svg/";
+  const base = "https://cdn.jsdelivr.net/gh/blackxs0001s/iconx@main/svg/";
 
   document.querySelectorAll("[class*='ji-']").forEach(el => {
-
-    const cls = [...el.classList]
-      .find(c => c.startsWith("ji-") && c !== "ji");
-
+    const cls = [...el.classList].find(c => c.startsWith("ji-"));
     if (!cls) return;
 
-    const icon = cls.replace("ji-", "");
+    const icon = cls.substring(3); // ji-View-1 -> View-1
 
     el.style.display = "inline-block";
     el.style.width = "1em";
     el.style.height = "1em";
-    el.style.background = "currentColor";
-
-    el.style.mask =
-      `url(${base}${icon}.svg) center / contain no-repeat`;
-
-    el.style.webkitMask =
-      `url(${base}${icon}.svg) center / contain no-repeat`;
-
+    el.style.backgroundColor = "currentColor";
+    el.style.verticalAlign = "-0.125em";
+    el.style.mask = `url("${base}${icon}.svg") center / contain no-repeat`;
+    el.style.webkitMask = `url("${base}${icon}.svg") center / contain no-repeat`;
   });
-
 })();
